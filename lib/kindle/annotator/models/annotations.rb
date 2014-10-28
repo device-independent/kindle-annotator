0ab21b5e4dd13d5ba13bc1d7612c6e3d4a2296ac
Nate Klaiber <nate@theklaibers.com>
2014-10-28 13:59:39 -0400

    Adding the `Annotations` model and specs

--- lib/kindle/annotator/models/annotations.rb
+++ lib/kindle/annotator/models/annotations.rb
@@ -1,11 +1,38 @@
+require File.expand_path('../../models/annotation', __FILE__)
+
 module Kindle
   module Annotator
     module Models
       class Annotations
+        AnnotationNotFoundError = Class.new(StandardError)
+
         include Enumerable
 
         def initialize(collection)
           @collection = Array(collection)
+        end
+
+        def each(&block)
+          annotations_collection.each(&block)
+        end
+
+        def find_by_id(id)
+          self.select { |record| record.id == id.to_s }.first
+        end
+
+        def find_by_id!(id)
+          record = self.find_by_id(id)
+          raise AnnotationNotFoundError.new('Annotation Not Found') unless record
+          record
+        end
+
+        def find_all_by_book_id(book_id)
+          self.select { |record| record.book_id == book_id.to_s }
+        end
+
+        private
+        def annotations_collection
+          @collection.map { |record| Annotation.new(record) }
         end
       end
     end
---
+++ spec/unit/models/annotations_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe Kindle::Annotator::Models::Annotations do
+  let(:annotations_file) do
+    File.read(File.expand_path('../../../data/annotations.json', __FILE__))
+  end
+
+  let(:annotations_collection) do
+    MultiJson.load(annotations_file)
+  end
+
+  subject { described_class.new(annotations_collection) }
+
+  describe ".find_all_by_book_id" do
+    it "returns the annotations by specified book_id" do
+      records = subject.find_all_by_book_id('accdd')
+    end
+  end
+
+  describe ".find_by_id" do
+    it "returns the annotation by it's ID" do
+      record = subject.find_by_id('vbfgh')
+      expect(record).not_to be_nil
+    end
+
+    it "returns nil if the annotation cannot be found" do
+      record = subject.find_by_id('notfound')
+      expect(record).to be_nil
+    end
+  end
+
+  describe ".find_by_id!" do
+    it "raises an exception if no annotation is found" do
+      expect { subject.find_by_id!('notfound') }.to raise_error(Kindle::Annotator::Models::Annotations::AnnotationNotFoundError)
+    end
+  end
+
+  context "with no records" do
+    let(:annotations_collection) { [] }
+
+    it "returns 0 for the #count" do
+      expect(subject.count).to eq(0)
+    end
+
+    it "returns false for #any?" do
+      expect(subject.any?).to be_falsey
+    end
+  end
+
+  context "with a single record" do
+    let(:annotations_collection) do
+      [MultiJson.load(annotations_file).first]
+    end
+
+    it "returns 1 for the #count" do
+      expect(subject.count).to eq(1)
+    end
+
+    it "returns true for #any?" do
+      expect(subject.any?).to be_truthy
+    end
+  end
+
+  context "with 2 records" do
+    it "returns 2 for the #count" do
+      expect(subject.count).to eq(2)
+    end
+
+    it "returns true for #any?" do
+      expect(subject.any?).to be_truthy
+    end
+  end
+end
